949d7f3b4b6e323cdde0c285eed04e6244f2fb6a
miroshnik <miroshnik@gmail.com>
2020-05-03 14:20:40 +0300

    Improve migration template

--- template/backend/storage/db/templates/migration.js
+++ template/backend/storage/db/templates/migration.js
@@ -1,12 +1,13 @@
 exports.up = async (knex) => {
-	// return knex.schema.createTable('table_name', (table) => {
-	// 	table.bigIncrements('id').unsigned().primary()
-	// 	table.bigint('some_id').unsigned().notNullable().references('id').inTable('some_table')
-	//
-	// 	table.unique(['field1', 'field2'])
-	// })
+  // await knex.schema.createTable('table_name', (table) => {
+  //   table.bigIncrements('id').unsigned().primary()
+  //   table.bigint('some_id').unsigned().notNullable()
+  //     .references('id').inTable('some_table').onDelete('cascade').onUpdate('cascade')
+  //
+  //   table.unique(['field1', 'field2'])
+  // })
 }
 
 exports.down = async (knex) => {
-	// return knex.schema.dropTable('table_name')
+  // await knex.schema.dropTable('table_name')
 }
